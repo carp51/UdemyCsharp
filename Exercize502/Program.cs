9a87fd0366106dab91e7d169900376135afe522c
carp51 <kitami2250@gmail.com>
2024-12-07 20:57:27 +0900

    first commit

--- Exercize502/Program.cs
+++ Exercize502/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[] d = new double[3];
+d[0] = 0.2;
+d[1] = 1.4;
+d[2] = -0.8;
+
+for(int i = 0; i < d.Length; i++)
+{
+    Console.WriteLine("d[{0}]={1}", i, d[i]);
+}
