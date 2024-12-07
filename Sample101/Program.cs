628dc7c0304f1559ddbb1bd3396d7af4c2bc5e77
carp51 <kitami2250@gmail.com>
2024-12-07 20:41:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample101/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Sample101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i + 1;
+            }
+            
+            foreach(int i in a)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
